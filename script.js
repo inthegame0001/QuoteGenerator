6520796f004dad5b2daeec75fb7c0a0bb61af7c3
python <inthegame001@outlook.com>
2023-02-18 14:24:15 +0330

    This is what our final program looks like, in the next versions, we will just work on the source code and make it easier to read.

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 //console.log(quoteContainer);
 let apiQuotes = [];
+function loading() {
+  loader.hidden = false;
+  quoteText.hidden = true;
+  authorText.hidden = true;
+}
+function complete() {
+  loader.hidden = true;
+  quoteText.hidden = false;
+  authorText.hidden = false;
+}
 function newQuote() {
+  loading();
   const quoto = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   authorText.textContent = quoto.author != null ? quoto.author : "Unknown";
   quoteText.textContent = quoto.text;
@@ -14,9 +26,11 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  complete();
   console.log(quoto);
 }
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const respose = await fetch(apiUrl);
