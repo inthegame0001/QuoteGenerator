75cb79211023d1b27b9685974e79ccc404f03313
python <inthegame001@outlook.com>
2023-02-18 16:55:49 +0330

    I tried to organize script.js (v1).

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+// Selecting HTML Elements by using their unique ID
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
-//console.log(quoteContainer);
-let apiQuotes = [];
-function loading() {
-  loader.hidden = false;
-  quoteText.hidden = true;
-  authorText.hidden = true;
+let recievedFromAPI = [];
+
+async function getQuotesFromAPI() {
+  startLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const respose = await fetch(apiUrl);
+    recievedFromAPI = await respose.json();
+    sendNewQuoteFromRecievedQuoteArray();
+  } catch (error) {
+    // alert(error);
+  }
 }
-function complete() {
-  loader.hidden = true;
-  quoteText.hidden = false;
-  authorText.hidden = false;
-}
-function newQuote() {
-  loading();
-  const quoto = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+function sendNewQuoteFromRecievedQuoteArray() {
+  startLoadingSpinner();
+  const quoto =
+    recievedFromAPI[Math.floor(Math.random() * recievedFromAPI.length)];
   authorText.textContent = quoto.author != null ? quoto.author : "Unknown";
   quoteText.textContent = quoto.text;
   if (quoto.text.length > 100) {
@@ -26,25 +30,26 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-  complete();
-  console.log(quoto);
+  stopLoadingSpinner();
 }
-async function getQuotes() {
-  loading();
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const respose = await fetch(apiUrl);
-    apiQuotes = await respose.json();
-    newQuote();
-  } catch (error) {
-    alert(error);
-  }
-}
-getQuotes();
 
-function tweetQuote() {
+function tweetTheQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
 }
-newQuoteBtn.addEventListener("click", newQuote);
-twitterBtn.addEventListener("click", tweetQuote);
+
+function startLoadingSpinner() {
+  loader.hidden = false;
+  quoteText.hidden = true;
+  authorText.hidden = true;
+}
+
+function stopLoadingSpinner() {
+  loader.hidden = true;
+  quoteText.hidden = false;
+  authorText.hidden = false;
+}
+
+newQuoteBtn.addEventListener("click", sendNewQuoteFromRecievedQuoteArray);
+twitterBtn.addEventListener("click", tweetTheQuote);
+getQuotesFromAPI();
